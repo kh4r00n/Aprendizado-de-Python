fdad8ee3bff9ba4ab168c59876264f0ad9d2cdaa
kh4r00n <82526635+kh4r00n@users.noreply.github.com>
2021-05-02 08:51:06 -0300

    Add files via upload
    Alistamento militar

---
+++ ex039.py
@@ -0,0 +1,17 @@
+from datetime import date
+atual = date.today().year
+nasc = int(input('Ano de nascimento: '))
+idade = atual - nasc
+print('Quem nasceu em {} tem {} anos em {}'.format(nasc, idade, atual))
+if idade == 18:
+    print('Você tem que se alistar imediatamente.')
+elif idade < 18:
+    saldo = atual + 18
+    print('Ainda faltam {} anos para o alistamento'.format(saldo))
+    ano = atual + saldo
+    print('Seu alistamento será em {}'.format(ano))
+elif idade > 18:
+    saldo = idade - 18
+    print('Você já deveria ter se alistado há {} anos'.format(saldo))
+    ano = atual - saldo
+    print('Seu alistamento foi em {}'.format(ano))
