7e883c8f06e072ae9e9c79b1b3cd5d14645a9695
Phylyp Cavalcante <82526635+kh4r00n@users.noreply.github.com>
2021-09-23 18:29:10 -0300

    ClassificandoTriângulos

--- ex042.py
+++ ex042.py
@@ -1,3 +1,12 @@
+"""
+EXERCÍCIO 042: Analisando Triângulos v2.0
+Refaça o EXERCÍCIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+- Equilátero: todos os lados iguais
+- Isósceles: dois lados iguais
+- Escaleno: todos os lados diferentes
+"""
+
+
 r1 = float(input('Primeiro segmento: '))
 r2 = float(input('Segundo segmento: '))
 r3 = float(input('Terceiro segmento: '))
